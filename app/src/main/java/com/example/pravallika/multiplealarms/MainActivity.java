d86d8599c570863335cb5043ddb795fb2250f97f
Riten Vithlani <vithlani.riten@gmail.com>
2017-02-19 21:26:21 -0600

    Home Page UI

--- app/src/main/java/com/example/pravallika/multiplealarms/MainActivity.java
+++ app/src/main/java/com/example/pravallika/multiplealarms/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.pravallika.multiplealarms;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnAlarm = (Button) findViewById(R.id.btn_alarm);
+        Button btnMultipleAlarm = (Button) findViewById(R.id.btn_multiple_alarm);
+        Button btnReminder = (Button) findViewById(R.id.btn_reminder);
+
+        btnAlarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent alarmIntent = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(alarmIntent);
+            }
+        });
+
+        btnMultipleAlarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent multipleAlarmIntent = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(multipleAlarmIntent);
+            }
+        });
+
+        btnReminder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent reminderIntent = new Intent(MainActivity.this, ReminderActivity.class);
+                startActivity(reminderIntent);
+            }
+        });
+
     }
 }
