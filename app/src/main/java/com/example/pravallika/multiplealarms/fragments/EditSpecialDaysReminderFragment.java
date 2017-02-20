da2183c59095752a968fd1d5b80a47b74c007d3c
Riten Vithlani <vithlani.riten@gmail.com>
2017-02-20 15:19:59 -0600

    Add FAB Reminder Page

--- app/src/main/java/com/example/pravallika/multiplealarms/fragments/EditSpecialDaysReminderFragment.java
+++ app/src/main/java/com/example/pravallika/multiplealarms/fragments/EditSpecialDaysReminderFragment.java
@@ -6,15 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ListView;
 
 import com.example.pravallika.multiplealarms.R;
-import com.example.pravallika.multiplealarms.adapters.ReminderAdapter;
-import com.example.pravallika.multiplealarms.beans.Reminder;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 /**
@@ -32,26 +25,6 @@ public class EditSpecialDaysReminderFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_spl_days_reminder_form, container, false);
-
-        // Create a list of words
-        List<Reminder> reminders = new ArrayList<Reminder>();
-        // Fetch the values from the database
-        reminders.add(new Reminder("Ram birthday", "Mar 6", "12:00 AM"));
-        reminders.add(new Reminder("Parents Marriage day", "Sep 10", "8:00 AM"));
-
-        ReminderAdapter reminderAdapter =
-                new ReminderAdapter(getActivity(), reminders);
-
-        ListView listView = (ListView) rootView.findViewById(R.id.reminder_list);
-        listView.setAdapter(reminderAdapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Fragment editSpecialDaysReminderFragment = new EditSpecialDaysReminderFragment();
-            }
-        });
-
         return rootView;
     }
 
