38102e9677eb1489941a53cf9b7edfb68b40f16b
Riten Vithlani <vithlani.riten@gmail.com>
2017-04-30 17:34:47 -0500

    Bug fix - Notification triggering immediately on toggle ON

--- app/src/main/java/com/example/pravallika/multiplealarms/helpers/NotificationHelper.java
+++ app/src/main/java/com/example/pravallika/multiplealarms/helpers/NotificationHelper.java
@@ -29,17 +29,20 @@ public class NotificationHelper {
     }
 
     public static void createNotification(Context context, long triggerTimeInMillis, String notificationMessage, MultipleAlarmConstants.FeatureType featureType) {
-        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-        int requestCode = Utility.getUniqueRequestCode(triggerTimeInMillis, featureType);
+        // If you start pending intent on the time that has already elapsed, then it triggers immediately. Hence this condition
+        if (triggerTimeInMillis > System.currentTimeMillis()) {
+            AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+            int requestCode = Utility.getUniqueRequestCode(triggerTimeInMillis, featureType);
 
-        Intent intent = new Intent(context, NotificationReceiver.class);
-        intent.putExtra("notificationMessage", notificationMessage);
-        intent.putExtra("requestCode", requestCode);
-        intent.putExtra("notificationTitle", featureType.title());
+            Intent intent = new Intent(context, NotificationReceiver.class);
+            intent.putExtra("notificationMessage", notificationMessage);
+            intent.putExtra("requestCode", requestCode);
+            intent.putExtra("notificationTitle", featureType.title());
 
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, requestCode,
-                intent, PendingIntent.FLAG_UPDATE_CURRENT);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context, requestCode,
+                    intent, PendingIntent.FLAG_UPDATE_CURRENT);
 
-        alarmManager.set(AlarmManager.RTC_WAKEUP, triggerTimeInMillis, pendingIntent);
+            alarmManager.set(AlarmManager.RTC_WAKEUP, triggerTimeInMillis, pendingIntent);
+        }
     }
 }
